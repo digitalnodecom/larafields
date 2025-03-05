13d9e4c3b763d6731b94d89b9fa176c86c1f1778
KIKOmanasijev <manasijevhristijan@gmail.com>
2025-03-05 20:03:23 +0100

    feat: implement api PEST tests

---
+++ tests/CreatesApplication.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        // This is a simplified version for testing purposes
+        // In a real Laravel application, this would create and bootstrap the application
+        return null;
+    }
+}
